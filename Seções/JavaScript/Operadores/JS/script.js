215c23f679e510736a80458be3ad06bc21a8c301
Felipe Destro <122558891+FelipeDestroo@users.noreply.github.com>
2023-12-15 16:30:31 -0300

    Tipos de dados e operadores

---
+++ Seções/JavaScript/Operadores/JS/script.js
@@ -0,0 +1,58 @@
+// 1 - Number
+console.log(typeof 4)
+console.log(typeof 2.87)
+console.log(typeof -55)
+
+// 2 - Operações Aritméticas
+console.log(2 + 9)
+console.log(2 - 9)
+console.log(2 * (8 + 9))
+console.log(10 / 2 + (2 + 3))
+
+// 3 - Special Numbers
+console.log(typeof Infinity)
+console.log(typeof -Infinity)
+// console.log(4 * hello)
+console.log(typeof NaN)
+
+// 4 - String
+console.log("Hello World!");
+console.log(`Mais um texto`);
+console.log(typeof `144`);
+
+// 5 - Símbolos especiais em string
+console.log("Testando a \nquebra de linha");
+console.log("Espaçamento de \t tab");
+
+// 6 - Concatenação
+console.log("Olá," + " mundo" + "!");
+console.log(`Testando` + ` com` + ` crase`);
+
+// 7 - Interpolação
+console.log(`A soma de 5 * 2 é: ${5 * 2}`);
+
+// 8 - Boolean
+console.log(true);
+console.log(false);
+console.log(30 > 2);
+console.log(90<100);
+console.log(typeof true);
+
+// 9 - Comparações
+console.log(5 <= 5);
+console.log(5 < 5);
+console.log(10 == 10);
+console.log(10 == 9);
+console.log(20 != 20);
+
+//  10 - Idêntico
+console.log(9 == "9") // !
+console.log(9 === "9")
+console.log(9 != "9")
+console.log(9 !== "9")
+
+// 11 - Operadores lógicos
+
+
+
+
