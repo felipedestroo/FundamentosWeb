537628382b3334e18b079fbb70d1c2fe13d11cd5
Felipe Destro <122558891+FelipeDestroo@users.noreply.github.com>
2023-12-20 08:46:02 -0300

    commit

---
+++ Seções/JavaScript/Estruturas/js/script.js
@@ -0,0 +1,36 @@
+// 1 - Variáveis
+let nome = "Felipe";
+
+console.log(nome);
+
+nome = "Felipe Destro Munhos Silva"
+
+console.log(nome);
+
+const idade = 19 //Const não muda de valor
+
+console.log(idade)
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+// 2 - Mais sobre variáveis
+
+// let 2teste = "Inválido"
+// let @teste = "Inválido"
+
+let a = 10, b = 20, c = 30
+console.log(a, b , c)
+
+const nomecompleto = "Felipe Destro";
+const nomeCompleto = "Munhos Silva";
+
+console.log(nomecompleto)
+console.log(nomeCompleto)
+
+//simbolos permitidos para criação de variaveis 
+let _teste = "ok" 
+let $teste = "ok"
+
+console.log(_teste)
+console.log($teste)
