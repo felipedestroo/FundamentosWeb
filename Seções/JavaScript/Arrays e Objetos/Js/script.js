3539453b45185dbe60b1b5f1099120b7e2a1788f
Felipe Destro <122558891+FelipeDestroo@users.noreply.github.com>
2023-12-27 17:11:41 -0300

    updated js

--- Seções/JavaScript/Arrays e Objetos/Js/script.js
+++ Seções/JavaScript/Arrays e Objetos/Js/script.js
@@ -54,36 +54,115 @@
 
 // 6 - Criando e deletando propriedades
 
-const car = {
-    engine: 2.0,
-    brand: "VW",
-    model: "Tiguan",
-    km: 2000,
-};
+// const car = {
+//     engine: 2.0,
+//     brand: "VW",
+//     model: "Tiguan",
+//     km: 2000,
+// };
 
-console.log(car)
+// console.log(car)
 
-car.doors = 4;
-console.log(car)
+// car.doors = 4;
+// console.log(car)
 
-delete car.km;
+// delete car.km;
 
-console.log(car);
+// console.log(car);
 
-// 7 - Mais sobre objetos
+// // 7 - Mais sobre objetos
 
-const obj = {
-    a: "teste",
-    b: true,
-};
+// const obj = {
+//     a: "teste",
+//     b: true,
+// };
 
-console.log(obj instanceof Object);
+// console.log(obj instanceof Object);
 
-const obj2 = {
-    c: [],
-};
+// const obj2 = {
+//     c: [],
+// };
 
-Object.assign(obj2, obj)
+// Object.assign(obj2, obj)
 
-console.log(obj2);
-console.log(obj);
+// console.log(obj2);
+// console.log(obj);
+
+// // 8 - Conhecendo melhor os objetos 
+// console.log(Object.keys(obj));
+// console.log(Object.keys(obj2));
+// console.log(Object.keys(car));
+
+// console.log(Object.entries(car));
+
+// // 9 - Mutação
+// const a = {
+//     name: "Felipe Destro",
+// };
+
+// const b = a;
+
+// console.log(a);
+// console.log(b);
+
+// a.age = 19;
+
+// console.log(a);
+// console.log(b);
+
+// delete b.age
+
+// console.log(a);
+// console.log(b);
+
+// 10 - Loop em array
+
+const users = ["Felipe", "Nicolas", "Pedro", "Henrique"]
+
+for(let i = 0; i < users.length; i++) {
+    console.log(`Listando o usuário: ${users[i]}`);
+}
+
+// 11 - Push e pop
+
+
+// Push e pop correspondem ao fim 
+
+const array = ["a", "b", "c"]
+
+array.push("d");
+
+console.log(array);
+console.log(array.length);
+
+array.pop();
+
+console.log(array);
+
+const itemRemovido = array.pop()
+
+console.log(itemRemovido);
+
+console.log(array);
+
+array.push("z", "x", "y");
+
+console.log(array);
+
+// 12 - Shift e unshift
+
+// Shift e unshift corresponde ao início
+
+const letters = ["a", "b", "c", "d"]
+
+const letter = letters.shift();
+
+console.log(letter);
+console.log(letters);
+
+letters.unshift("f", "g", "h");
+
+letters.unshift("z");
+
+console.log(letters);
+
