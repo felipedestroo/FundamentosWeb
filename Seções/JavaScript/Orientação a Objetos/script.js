c1c1ed866b4b52149326c9c1972c804e9b9ecbb4
Felipe Destro <122558891+FelipeDestroo@users.noreply.github.com>
2024-01-10 16:50:49 -0300

    Update script.js

--- Seções/JavaScript/Orientação a Objetos/script.js
+++ Seções/JavaScript/Orientação a Objetos/script.js
@@ -1,35 +1,112 @@
 // 1 - Métodos
 
-const animal = {
-    nome: "Max",
-    latir: function() {
-        console.log("Au au")
-    },  
-};
+// const animal = {
+//     nome: "Max",
+//     latir: function() {
+//         console.log("Au au")
+//     },  
+// };
 
-console.log(animal.nome);
+// console.log(animal.nome);
 
-animal.latir();
+// animal.latir();
 
-// 2 - Aprofundando em métodos
+// // 2 - Aprofundando em métodos
 
-const pessoa = {
+// const pessoa = {
     
-    nome: "Felipe",
+//     nome: "Felipe",
 
-    getNome: function () {
-        return this.nome
-    },      
+//     getNome: function () {
+//         return this.nome
+//     },      
 
-    setNome: function(novoNome) {
-        this.nome = novoNome;
-    },
+//     setNome: function(novoNome) {
+//         this.nome = novoNome;
+//     },
 
+// };
+
+// console.log(pessoa.nome);
+// console.log(pessoa.getNome());
+
+// pessoa.setNome("Destro");
+
+// console.log(pessoa.getNome());
+
+// // 3  - Prototype
+
+// const text = "abc"
+
+// console.log(Object.getPrototypeOf(text));
+
+// const bool = true;
+
+// console.log(Object.getPrototypeOf(bool));
+
+// const arr = [];
+
+// console.log(arr.lenght);
+
+// console.log(Object.getPrototypeOf(arr));
+
+// // 4 - Continuação prototype
+
+// const myObject = {
+//     a: "b",
+// }
+
+// console.log(Object.getPrototypeOf(myObject));
+
+// console.log(Object.getPrototypeOf(myObject) === Object.prototype);
+
+// const mySecondObject = Object.create(myObject);
+
+// console.log(mySecondObject);
+
+// console.log(mySecondObject.a);
+
+// console.log(Object.getPrototypeOf(mySecondObject) === myObject);
+
+// 5 - Classes básicas
+
+const cachorro = {
+    raca: null,
+    patas: 4,
 };
 
-console.log(pessoa.nome);
-console.log(pessoa.getNome());
+const pastorAlemao = Object.create(cachorro);
 
-pessoa.setNome("Destro");
+pastorAlemao.raca = "Pastor Alemão"
 
-console.log(pessoa.getNome());
+console.log(pastorAlemao);
+
+console.log(pastorAlemao.patas);
+
+const bulldog = Object.create(cachorro);
+
+bulldog.raca = "Bulldog";
+
+console.log(bulldog);
+
+// 6 - Funções construtoras
+
+function criarCachorro(nome, raca) {
+    const cachorro = Object.create({})
+
+    cachorro.nome = nome
+    cachorro.raca = raca
+
+    return cachorro;
+
+}
+
+const bob = criarCachorro("Max", "Husky");
+
+console.log(bob);
+
+const thor = criarCachorro("Thor", "Pinscher");
+
+console.log(thor);
+
+console.log(Object.getPrototypeOf(thor));
