0ededa6e6f47dbf4c7bc305d44d39c4b073eb7a9
Felipe Destro <122558891+FelipeDestroo@users.noreply.github.com>
2024-03-05 17:08:44 -0300

    Adicionando prototype no script.js

--- Seções/JavaScript/Orientação a Objetos/script.js
+++ Seções/JavaScript/Orientação a Objetos/script.js
@@ -128,3 +128,65 @@ Cachorro.prototype.uivar = function() {
 
 console.log(cachorro.prototype);
 husky.uivar();
+
+// 9 - Classes ES6
+class CachorroClasse {
+    constructor(nome, raca) {
+        this.nome = nome;
+        this.raca = raca;   
+    }
+}
+
+const jeff = new CachorroClasse("Jeff", "Labrador");
+
+console.log(jeff);
+
+console.log(Object.getPrototypeOf(jeff));
+
+
+// 10 - Mais sobre classes
+
+class Caminhao {
+    constructor(eixos, cor) {
+        this.eixos = eixos;
+        this.cor = cor; 
+    }
+
+    descreverCaminhao() {
+        console.log(`Este caminhão tem ${this.eixos} eixos e é da cor ${this.cor}`);
+    }
+}
+
+const scania = new Caminhao(6, "Vermelha")
+
+console.log(scania);
+
+scania.descreverCaminhao();
+
+const c2 = new Caminhao(4, "Preta")
+
+console.log(c2);
+console.log(c2.motor);
+
+Caminhao.prototype.motor = 4.0;
+
+const c3 = new Caminhao(6, "Azul");
+
+console.log(c3.motor);
+
+ // 11 - Override
+ class Humano {
+    constructor(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+ }
+
+ const felipe = new Humano("Felipe", 20);
+ console.log(felipe);
+
+Humano.prototype.idade = "Não definida"
+
+console.log(felipe.idade);
+
+console.log(Humano.prototype.idade);
