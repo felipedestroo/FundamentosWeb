90aa25849e17bbfd7d594c1ab7d52115288820b9
Felipe Destro <122558891+FelipeDestroo@users.noreply.github.com>
2024-04-23 08:52:06 -0300

    Resolvendo várias promises

--- Seções/JavaScript/Async/js/script.js
+++ Seções/JavaScript/Async/js/script.js
@@ -57,3 +57,23 @@ const b = checkNumber(10);
 a.then((v) => console.log(`O resultado é ${v}`)).catch((err) => console.log(`Um erro ocorreu: ${err}`));
 
 b.then((v) => console.log(`O resultado é ${v}`)).catch((err) => console.log(`Um erro ocorreu: ${err}`));
+
+// 6 - Resolvendo várias promises
+
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(function(){
+        resolve(10)
+    }, 3000)
+})
+
+const p2 = Promise.resolve(10 + 10)
+
+const p3 = new Promise((resolve, reject) => {
+    if(30 > 10){
+        resolve(30)
+    } else {
+        reject("Erro!")
+    }
+})
+
+Promise.all([p1, p2, p3]).then((values) => console.log(values));
