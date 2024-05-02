b57e0ddde6108bb6e32ecc5283c99cf4f9c1ac60
Felipe Destro <122558891+FelipeDestroo@users.noreply.github.com>
2024-05-02 11:59:57 -0300

    Altura e Largura

--- Seções/JavaScript/Dom/js/script.js
+++ Seções/JavaScript/Dom/js/script.js
@@ -86,3 +86,12 @@ console.log(firstLink.getAttribute("href"));
 
 firstLink.setAttribute("target", "_blank");
 
+// 11 0 altura e largura
+
+const footer = document.querySelector("footer")
+
+console.log(footer.offsetWidth);
+console.log(footer.offsetHeight);
+
+console.log(footer.clientWidth);
+console.log(footer.clientHeight);
