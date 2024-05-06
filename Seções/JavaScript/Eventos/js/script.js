8fa2a5a1ed02a9bbb85e95611fea1ddf18ff60f6
Felipe Destro <122558891+FelipeDestroo@users.noreply.github.com>
2024-05-06 15:09:53 -0300

    Removendo eventos

--- Seções/JavaScript/Eventos/js/script.js
+++ Seções/JavaScript/Eventos/js/script.js
@@ -6,4 +6,19 @@ btn.addEventListener("click", function(){
     console.log("Clicou em mim");
 });
 
+// 2 - removendo evento
 
+const secondBtn = document.querySelector("#btn");
+
+function imprimirMensagem() {
+    console.log("Click!");
+};
+
+secondBtn.addEventListener("click", imprimirMensagem);
+
+const thirdBtn = document.querySelector("#other-btn");
+
+thirdBtn.addEventListener("click", () => {
+    console.log("Evento removido");
+    secondBtn.removeEventListener("click", imprimirMensagem);
+});
