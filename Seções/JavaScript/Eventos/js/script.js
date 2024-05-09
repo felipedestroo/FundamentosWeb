5c37ffac67a2239dea0b2d8297df1e01545d5395
Felipe Destro <122558891+FelipeDestroo@users.noreply.github.com>
2024-05-09 15:00:49 -0300

    Debounce

--- Seções/JavaScript/Eventos/js/script.js
+++ Seções/JavaScript/Eventos/js/script.js
@@ -114,3 +114,27 @@ window.addEventListener("beforeunload", (e) => {
     e.preventDefault();
     e.returnValue = "teste";
 })
+
+// 12 - Debounce
+
+const debounce = (f, delay) => {
+
+    let timeout
+
+    return (...arguments) => {
+        if(timeout) {
+            clearTimeout(timeout)
+        }
+
+        timeout = setTimeout(() => {
+            f.apply(arguments)
+        }, delay);
+    };
+};
+
+window.addEventListener(
+    "mousemove",
+    debounce(() => {
+        console.log("Executando a cada 400ms");
+    }, 400)
+);
